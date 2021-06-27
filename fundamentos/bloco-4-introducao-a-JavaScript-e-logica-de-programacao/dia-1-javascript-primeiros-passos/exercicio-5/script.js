c16aa8a008b6fce0fa5241c2059f77b3d7ac172e
Luana Eugênia <luaeugenia1309@outlook.com>
2021-06-27 06:12:41 -0300

    Programa que verifica se as retas podem formar um triângulo corrigido, agora um erro é retornado caso uma reta não tenha um valor positivo

--- fundamentos/bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5/script.js
+++ fundamentos/bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5/script.js
@@ -3,12 +3,16 @@ const b = 13
 const c = 15
 let eUmTriangulo = false
 
-if (a > b - c && a < b + c) {
-  if (b > a - c && b < a + c) {
-    if (c > a - b && c < a + b) {
-      eUmTriangulo = true
+if (a <= 0 || b <= 0 || c <= 0) {
+  console.log('ERRO: Todos os ângulos devem ter um valor maior que 0.')
+} else {
+  if (a > b - c && a < b + c) {
+    if (b > a - c && b < a + c) {
+      if (c > a - b && c < a + b) {
+        eUmTriangulo = true
+      }
     }
   }
+  
+  console.log(eUmTriangulo)
 }
-
-console.log(eUmTriangulo)
