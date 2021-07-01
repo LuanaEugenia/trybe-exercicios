9b21b6da19e385c0001e0e96c15d5e43bde11492
Luana Eugênia <luaeugenia1309@outlook.com>
2021-06-30 22:12:19 -0300

    Foi adicionada uma função que altera a cor de um elemento a partir do seu id.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/exercicio-1/scripts/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/exercicio-1/scripts/script.js
@@ -3,4 +3,10 @@ function adicionarConteudo(id, conteudo) {
   elemento.innerText = conteudo;
 }
 
-adicionarConteudo('textP', 'Olá, eu me chamo Luana Eugênia, eeee Tô na Trybe!');
+function alterarBackground(id, cor) {
+  const elemento = document.getElementById(id);
+  elemento.style.backgroundColor = cor
+}
+
+adicionarConteudo('textP', 'Olá, eu me chamo Luana Eugênia, eeee Tô na Trybe!')
+alterarBackground('main-content', 'rgb(76,164,109)');
