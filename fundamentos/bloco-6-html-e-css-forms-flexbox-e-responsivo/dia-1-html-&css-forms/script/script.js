6fbbb9629ff28b98e7f13650f6c981956efe1f3b
Luana Eugênia <luaeugenia1309@outlook.com>
2021-07-15 19:01:59 -0300

    Requisito Date, criado pela trybe.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-&css-forms/script/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-&css-forms/script/script.js
@@ -30,7 +30,7 @@ const estados = [
 
 function adicionarEstados() {
   const select = document.getElementById('estados');
-  
+
   for (let i = 0; i < estados.length; i += 1) {
     const estado = estados[i];
     const option = document.createElement('option');
@@ -40,5 +40,50 @@ function adicionarEstados() {
     select.appendChild(option);
   }
 }
+
+function dateValidation(input, name) {
+  if (input.value.length === 0) {
+    return {
+      message: 'A data não foi preenchida!'
+    }
+  }
+
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+
+  if (!regex.test(input.value)) {
+    return {
+      message: 'Data: Formato inválido'
+    };
+  }
+
+  let splitted = input.value.split('/');
+  let day = splitted[0];
+  let month = splitted[1];
+  let year = splitted[2];
+
+  if (day < 0 || day > 30) {
+    return {
+      message: 'Dia inválido'
+    };
+  }
+
+  if (month < 0 || month > 12) {
+    return {
+      message: 'Mês inválido'
+    }
+  }
+
+  if (year < 0) {
+    return {
+      message: 'Ano inválido'
+    };
+  }
+
+  return true;
+}
+
+document.getElementById('buttonPrevent').addEventListener('click', function (event) {
+  event.preventDefault()
+});
 
 adicionarEstados();
