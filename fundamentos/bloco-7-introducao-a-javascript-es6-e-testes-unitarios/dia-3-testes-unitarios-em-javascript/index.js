1c3424b973f92a3ae85f9dec35d52fb01f745855
Luana Eugênia <luaeugenia1309@outlook.com>
2021-07-27 13:37:29 -0300

    Testes realizado com a função sum(a,  b).

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/index.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/index.js
@@ -10,5 +10,10 @@ function sum(a, b) {
 
 // implemente seus testes aqui
 
-const expected = sum(4,5);
-assert.strictEqual(expected, 10, '4 + 5 = 9');
+let response = sum(4, 5);
+assert.strictEqual(response, 9, '4 + 5 = 9');
+
+response = sum(0, 0);
+assert.strictEqual(response, 0, '0 + 0 = 0');
+
+assert.throws(() => sum(4, '5'), /Error: parameters must be numbers$/);
