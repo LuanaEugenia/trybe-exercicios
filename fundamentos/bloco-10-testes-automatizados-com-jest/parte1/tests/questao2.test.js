f2dd409259255fc6cf5d47231120e364e961b8b4
Luana <luana.san2015.ls@gmail.com>
2021-08-25 18:46:20 -0300

    Exercício 3 - A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array.

--- fundamentos/bloco-10-testes-automatizados-com-jest/parte1/tests/questao2.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/parte1/tests/questao2.test.js
@@ -1,14 +1,16 @@
 const {myRemove} = require('../questao2');
 
 describe('Questão 2', () => {
-  test('retorna o array [1, 2, 4]', () => {
-    const resultado  = myRemove([1, 2, 3, 4], 3);
+  test('remove um item do array e retorna um novo array sem aquele item', () => {
+    const array = [1, 2, 3, 4];
+    const resultado  = myRemove(array, 3);
 
     expect(resultado).toEqual([1, 2, 4]);
-    expect(resultado).not.toEqual([1, 2, 3, 4]);
+    expect(resultado).not.toEqual(array);
+    expect(array).toEqual([1, 2, 3, 4]);
   });
 
-  test('retorna o array [1, 2, 3, 4', () => {
+  test('quando o item não existe no array, o array é retornado sem alterações', () => {
     const resultado  = myRemove([1, 2, 3, 4], 5);
 
     expect(resultado).toEqual([1, 2, 3, 4]);
---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/parte1/tests/questao3.test.js
@@ -0,0 +1,19 @@
+const {myRemoveWithoutCopy} = require('../questao3');
+
+describe('Questão 3', () => {
+  test('remove um item do array e retorna um novo array sem aquele item', () => {
+    const array = [1, 2, 3, 4];
+    const resultado  = myRemoveWithoutCopy(array, 3);
+
+    expect(resultado).toEqual([1, 2, 4]);
+    expect(resultado).not.toEqual([1, 2, 3, 4]);
+    expect(array).not.toEqual([1, 2, 3, 4]);
+  });
+
+  test('quando o item não existe no array, o array é retornado sem alterações', () => {
+    const array = [1, 2, 3, 4];
+    const resultado  = myRemoveWithoutCopy(array, 5);
+
+    expect(resultado).toEqual(array);
+  });
+});
