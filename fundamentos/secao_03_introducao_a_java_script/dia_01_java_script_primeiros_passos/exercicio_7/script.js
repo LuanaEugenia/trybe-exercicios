c982ef86bc68412740c33c610e08e216d36974b9
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-03-28 23:06:35 -0300

    Exercício 1 - Calculadora.

---
+++ fundamentos/secao_03_introducao_a_java_script/dia_01_java_script_primeiros_passos/exercicio_7/script.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 2;
+let soma = a + b;
+let dividir = a / b;
+let subtrair = a - b;
+let multiplicar = a * b;
+let expo = a ** b;
+
+console.log(soma);
+console.log(dividir);
+console.log(subtrair);
+console.log(multiplicar);
+console.log(expo);
