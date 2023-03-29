7ba3df4518fae648cc14f885050857e87b988bfe
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-03-29 02:09:45 -0300

    Requisito 15- Calculando o lucro de uma empresa.

---
+++ fundamentos/secao_03_introducao_a_java_script/dia_01_java_script_primeiros_passos/exercicio_15/script.js
@@ -0,0 +1,9 @@
+let custoProduto = 20;
+let valorVenda = 50;
+let quantidadeDeProdutosVendidos = 1000;
+let lucro = (valorVenda - custoProduto) * quantidadeDeProdutosVendidos;
+
+console.log('****************************************');
+console.log(`O lucro da empresa será de R$${lucro}.`);
+console.log('****************************************');
+
