45d457f388d36ad7bc22c43307a135311720f75d
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-03-30 10:51:00 -0300

    Quantidade de números ímpares. Lista os números ímpares, caso encontre algum, caso não encontre nenhum imprime mensagem.

--- fundamentos/secao_03_introducao_a_java_script/dia_02_javascript_array_e_loop_for/exercicio_5/script.js
+++ fundamentos/secao_03_introducao_a_java_script/dia_02_javascript_array_e_loop_for/exercicio_5/script.js
@@ -1,22 +1,35 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let mediaAritmerica = 0;
-let maiorNumero = 0;
+let maiorNumero = numbers[0];
+let menorNumero = numbers[0];
+let quantidadeNumeroImpares = 0;
+let saoImpares = [];
+const mensagem = 'Não há no array, números ímpares.';
 
 for (let index = 0; index < numbers.length; index += 1) {
-        maiorNumero = numbers[index]
-        for (let index = 1; index < numbers.length; index +=1) {
-            if(maiorNumero < numbers[index]) {
-                maiorNumero = numbers[index];
-            }
-        }
-        mediaAritmerica = soma / index;
-        console.log(numbers[index]);
-        soma += numbers[index];
-        if(numbers[index] > maiorNumero) {
-            maiorNumero = numbers[index];
-        }
+    mediaAritmerica = soma / index;
+    console.log(numbers[index]);
+    soma += numbers[index];
+
+    if (maiorNumero < numbers[index]) {
+        maiorNumero = numbers[index];
     }
+
+    if (menorNumero > numbers[index]) {
+        menorNumero = numbers[index];
+    }
+
+    if (numbers[index] % 2 != 0) {
+        quantidadeNumeroImpares = quantidadeNumeroImpares + 1;
+        saoImpares.push(numbers[index]);
+    }
+}
+
+if (quantidadeNumeroImpares === 0) {
+    mensagem;
+}
+
 
 console.log('*******************************************************************');
 if (soma > 20) {
@@ -27,6 +40,9 @@ if (soma > 20) {
 
 console.log('*******************************************************************');
 console.log(`O resultado da soma entre os números do array é: ${soma}`);
-console.log({ mediaAritmerica });
-console.log(maiorNumero);
+console.log(`A média aritmética é: ${mediaAritmerica}`);
+console.log(`O maior número do Array: ${maiorNumero}`);
+console.log(`O menor número do Array: ${menorNumero}`);
+console.log(`A quantidade de números ímapares são: ${quantidadeNumeroImpares}.`);
+console.log(`Números ímpares: ${saoImpares}`);
 console.log('*******************************************************************');
