f02ffc022862c7b017e8d2f97ea418f08407b1dd
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-04 11:21:01 -0300

    Exercício - Imprimindo inormações do objeto.

--- fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_05/script.js
+++ fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_05/script.js
@@ -15,5 +15,16 @@ let leitor = {
 
 console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
 
+leitor.livrosFavoritos.push (
+    {
+        titulo: 'A Hora da Estrela',
+        autor: 'Clarice Lispector',
+        editora: 'Rocco',
+    },
+);
 
+console.table(leitor.livrosFavoritos);
+ 
+console.log('***********************************************************************************');
 
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
