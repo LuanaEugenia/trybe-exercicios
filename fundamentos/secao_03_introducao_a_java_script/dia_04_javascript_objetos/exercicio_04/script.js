2fc602b7b216bd6f0d3fedb49429e45b06767186
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-04 10:02:59 -0300

    Imprimindo valores de dois objetos de acordo com as chaves.

--- fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_04/script.js
+++ fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_04/script.js
@@ -2,20 +2,47 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
-  console.log('Bem-vinda, ' +info.personagem+ '!');
-  console.log('***************************************');
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
 
-  let novaChave = 'recorrente';
-  let valorChave = 'sim';
+console.log('Bem-vinda, ' + info.personagem + '!');
+console.log('***************************************');
 
-  addInfo(info, novaChave, valorChave);
+let novaChave = 'recorrente';
+let valorChave = 'sim';
 
-  function addInfo(objeto, chave, valor) {
+/*Chamada da função
+addInfo(info, novaChave, valorChave);
+
+//Função para adicionar um novo valor e uma nova chave ao objeto
+function addInfo(objeto, chave, valor) {
     objeto[chave] = valor;
-  }
+}
 
-  for(let i in info) {
-    console.log(i);
-  }
+//Todas as chaves do objeto
+for (let key in info) {
+    console.log(key);
+}
+
+console.log('***************************************');
+
+
+//Todos os valores das propriedades do objeto
+for (let key in info) {
+    console.log(info[key]);
+}*/
+
+
+for (let key in info) {
+    if ( key === 'recorrente' && info[key] === 'sim' && info2[key] === 'sim') {
+        console.log('Ambos Recorrente');
+    } else {
+        console.log(info[key] + ' e ' +info2[key]);
+    }
+}
