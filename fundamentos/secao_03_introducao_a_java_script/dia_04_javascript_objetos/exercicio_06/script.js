be2cb8e7e5ba449b4f202984732741ab07c5df97
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-04 12:12:59 -0300

    Função orderModifier modificada e implementada.

--- fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_06/script.js
+++ fundamentos/secao_03_introducao_a_java_script/dia_04_javascript_objetos/exercicio_06/script.js
@@ -42,14 +42,22 @@ function customerInfo(order) {
     let endereco = order.address.street;
     let numero = order.address.number;
     let apartment = order.address.apartment;
-    
+
     console.log('Olá ' + deliveryPerson + ', entrega para: ' +  name + ', Telefone: ' + telefone +', R. ' +  endereco + ', N°: ' + numero + ', AP: ' + apartment);
 }
 
-customerInfo(order);
+order.order.delivery.deliveryPerson = 'Luis Silva';
+order.order.pizza.marguerita.price = 25;
+order.order.pizza.pepperoni.price = 20;
+order.order.drinks.coke.price = 5;
 
 function orderModifier(order) {
-    // Adicione abaixo as informações necessárias.
+    let deliveryPerson = order.order.delivery.deliveryPerson;
+    let precoMarguerita = order.order.pizza.marguerita.price;
+    let precoPepperoni = order.order.pizza.pepperoni.price;
+    let precodrink = order.order.drinks.coke.price;
+
+    console.log('Olá ' + deliveryPerson + ', o valor total de seu pedido de margureita, pepperoni e coca-cola é R$' + (precoMarguerita + precoPepperoni + precodrink) + ',00.');
 }
 
 orderModifier(order);
