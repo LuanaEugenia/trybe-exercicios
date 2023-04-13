42230df64064a623f2e6c84e8c02c6a49ae7fd2e
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-12 23:49:03 -0300

    Criado um irmão para elemento.

---
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_02_javascript_trabalhando_com_elementos/exercicio_02/script.js
@@ -0,0 +1,7 @@
+    // Crie um irmão para `elementoOndeVoceEsta`.
+const elementoPai = document.getElementById('pai');
+const novoElemento = document.createElement('section');
+novoElemento.id = 'irmaoElementoOndeVoceEsta';
+elementoPai.appendChild(novoElemento);
+
+
