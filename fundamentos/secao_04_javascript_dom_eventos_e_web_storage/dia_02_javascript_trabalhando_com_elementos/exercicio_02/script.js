36ac8993f97527375e412d839d4fdac74cc7e691
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-12 23:49:50 -0300

    Foi criado um filho para o elemento.

--- fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_02_javascript_trabalhando_com_elementos/exercicio_02/script.js
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_02_javascript_trabalhando_com_elementos/exercicio_02/script.js
@@ -1,7 +1,12 @@
-    // Crie um irmão para `elementoOndeVoceEsta`.
+// Crie um irmão para `elementoOndeVoceEsta`.
 const elementoPai = document.getElementById('pai');
 const novoElemento = document.createElement('section');
 novoElemento.id = 'irmaoElementoOndeVoceEsta';
 elementoPai.appendChild(novoElemento);
 
+// Crie um filho para `elementoOndeVoceEsta`.
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
 
