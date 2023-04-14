59e427a9b231aadb5db57b772e40da25271e068d
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-13 21:19:41 -0300

    Uma função que, ao digitar na caixa de texto, altere o texto do elemento.

--- fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_03_javascript_eventos/exercicio_01/script.js
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_03_javascript_eventos/exercicio_01/script.js
@@ -11,16 +11,23 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
 const lidandoComTech = (event) => {
-    const elementoTech = document.querySelector('tech');
+    const elementoTech = document.querySelector('.tech');
     elementoTech.classList.remove('tech');
     event.target.classList.add('tech');
     input.value = '';
 }
+
 primeiroLi.addEventListener('click', lidandoComTech);
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
+input.addEventListener('input', (event) => {
+    const elementoTech = document.querySelector('.tech');
+    elementoTech.innerText = event.target.value;
+})
 
+segundoLi.addEventListener('click', lidandoComTech)
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
