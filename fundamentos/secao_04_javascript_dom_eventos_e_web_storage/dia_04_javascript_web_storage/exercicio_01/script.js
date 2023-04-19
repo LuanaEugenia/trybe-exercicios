4141b696677bcb3a333d853c669ac9efb0f54513
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-04-19 00:57:16 -0300

    Adicionado função setColorBackground e setColorText.

---
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_04_javascript_web_storage/exercicio_01/script.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  // Cor de fundo da tela;
+  const setColorBackgound = (color) => {
+    const content = document.querySelector('.content');
+    content.style.backgroundColor = color;
+  }
+  // Cor do texto;
+  const setColorText = (colorText) => {
+    const paragraph = document.querySelectorAll('.paragraph');
+    paragraph.style.color = colorText;
+  }
+  // Tamanho da fonte;
+  // Espaçamento entre as linhas do texto;
+  // Tipo da fonte (Font family).
+}
